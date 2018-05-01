955c54ba86e9b15d1d57933ca23233e82ca93f6d
Bruce Richardson <bjr@BMP.local>
2018-05-01 16:34:57 -0600

    removed stuff from rnlive for ios

--- ios/RCTLFLiveKit/RCTLFLiveKit/RCTStream.m
+++ ios/RCTLFLiveKit/RCTLFLiveKit/RCTStream.m
@@ -69,13 +69,6 @@
 		[self addSubview:self.containerView];
 	}
 
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC);
-	dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-		[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
-		[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];  // allow
-		[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
-	});
-
 	return self;
 }
 
@@ -117,12 +110,6 @@
     switch (status) {
         case AVAuthorizationStatusNotDetermined: {
             [AVCaptureDevice requestAccessForMediaType:AVMediaTypeAudio completionHandler:^(BOOL granted) {
-				dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC);
-				dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-					[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
-					[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];  // allow
-					[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
-				});
             }];
             break;
         }
